fc60b3112bb9b2065327f856ff1a8643c50feb8a
Krishna93951 <kkagile123@gmail.com>
2019-08-30 19:14:38 +0530

    1 file modified

--- every.js
+++ every.js
@@ -10,4 +10,33 @@ console.log(b.every(isBigEnough));
 
 console.log(a.every(x=>x<=10));
 console.log(b.every(x=>x<=1));
+
+
+// array.fill
+console.log(a.fill(0,1,4))
+console.log(b.fill(4,NaN,NaN))
+
+//Filter
+var fruits = ['apple', 'banana', 'grapes', 'mango', 'orange'];
+
+/**
+ * Filter array items based on search criteria (query)
+ */
+function filterItems(arr, query) {
+  return arr.filter(function(el) {
+      return el.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+  })
+}
+
+console.log(filterItems(fruits, 'ap')); 
+console.log(filterItems(fruits, 'an'));
+
+//find
+var a = [5, 12, 8, 130, 44];
+
+var found = a.find(function(element) {
+  return element < 10;
+});
+
+console.log(found);
 } 
