3af4f89f833b1d6914da640c8cd0c8bf2a67970d
Krishna93951 <kkagile123@gmail.com>
2019-08-30 17:48:27 +0530

    2 files modified

--- copywithin.js
+++ copywithin.js
@@ -1,4 +1,23 @@
-exports.bc = function bc{
+exports.bc = function bc() {
 
-     
-}
+ var a=[1,2,3,4]
+ //Target greater than length
+ var b= a.copyWithin(5,1,2);
+ console.log(b)
+
+ //Target positive and greater than start
+ var c= a.copyWithin(1,0,3);
+ console.log(c) //4 is trimmed 
+
+ //Target negative 
+ var d= a.copyWithin(-1,2,3);
+ console.log(d) //starts from ending
+
+ //Start is positive
+ var e= a.copyWithin(0,2,3);
+ console.log(e)
+
+ //Start is negative
+ var f= a.copyWithin(0,-1,1);
+ console.log(f)
+};
--- index.js
+++ index.js
@@ -4,12 +4,13 @@ import {s} from './Arrayfrom().js';
 import {b} from './Arrayfrom().js'; 
 import {k} from './Arrayfrom().js'; 
 import {ab} from './concat.js';
-import {bc} from './copywithin,js'; 
+import {bc} from './copywithin.js'; 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 s();
 b();
 k();
-ab();
+//ab();
+bc();
 
