38119071294b2c6c105279ae5836ddb7eb01228c
Krishna93951 <kkagile123@gmail.com>
2019-09-04 14:47:28 +0530

    1 file modified

--- arraymethods.js
+++ arraymethods.js
@@ -14,4 +14,40 @@ const index = fruits.findIndex(fruit => fruit === "mango");
 
 console.log(index);
 console.log(fruits[index]);
+
+//flat
+var a=[1,[2,3,[4,5,3,[5,6,7,[2]]]]]
+console.log(a.flat(Infinity))
+
+//map
+var numbers = [1, 4, 9];
+var roots = numbers.map(function(num) {
+return Math.sqrt(num)
+});
+console.log(roots);
+
+var map = Array.prototype.map;
+var a = map.call('Hello World', function(x) { 
+  return x.charAt(0); 
+});
+//console.log(a);
+
+var b= [, ,]
+var c=b.map(function(e){
+  return e
+});
+console.log(c)
+
+//console.log(['1','2','3'].map(parseInt))
+
+//flatMap
+var x=[1,2,3,4]
+var z= x.map(x=>[x*2])
+console.log(z)
+
+var f=x.flatMap(x=>[x*2])
+console.log(f)
+
+
 }
+
