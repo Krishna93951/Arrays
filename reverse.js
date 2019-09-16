52a0a410ad51ac2c352a276577e070f345e472c8
Krishna93951 <kkagile123@gmail.com>
2019-09-16 18:36:28 +0530

    1 file modified

--- reverse.js
+++ reverse.js
@@ -6,17 +6,23 @@ exports.l=function l()
 
   //reverse of single 
   var b=[1]
-  console.log(b.reverse())
+  //console.log(b.reverse())
 
   //reverse of an object
  const a = {0: 'a', 1: 3, 2: 'b', length: 3};
 
-console.log(a); // {0: 1, 1: 2, 2: 3, length: 3}
+//console.log(a); // {0: 1, 1: 2, 2: 3, length: 3}
 
 Array.prototype.reverse.call(a); //same syntax for using apply()
 
-console.log(a);
+//console.log(a);
 
+//shift
+var names = ["Andrew", "Edward", "Paul", "Chris" ,"John"];
+
+while( (i = names.shift()) !== undefined ) {
+    console.log(i);
+}
 
 
 }
