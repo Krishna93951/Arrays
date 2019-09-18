7ce3861d2bb456842aa6c10a2759b4c9e8b8dd1a
Krishna93951 <kkagile123@gmail.com>
2019-09-18 17:36:27 +0530

    1 file modified

--- unshift.js
+++ unshift.js
@@ -1,3 +1,10 @@
 exports.unshift=function unshift(){
-  
+  var num=[1,2,3]
+  console.log(num.unshift('kk'))
+  console.log(num)
+  //array in array
+
+  var a=[1,2,3,34]
+  console.log(a.unshift([20,10],[1]))
+  console.log(a)
 }
